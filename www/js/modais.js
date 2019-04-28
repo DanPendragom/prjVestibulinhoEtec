2f43c6cfcc83afa9e72ae0645b48ae36784f5b0e
yvolorenzo2019 <yvolorezo@gmail.com>
2019-04-28 12:22:22 +0900

    Modal Técnico em edificações

--- www/js/modais.js
+++ www/js/modais.js
@@ -3,6 +3,11 @@ function retorno()
 {
 
 }
-$(document).on('click','#opc2',function(){
+$(document).on('click','#manutencao',function(){
   navigator.notification.alert("O curso Técnico em Manutenção e Suporte em Informática tem por objetivo capacitar profissionais para fazer a instalação, configuração e manutenção de equipamentos de informática, a configuração e gerenciamento de redes de computadores, além de dar suporte na utilização dos mais diversos softwares e na identificação das necessidades de substituição ou atualização tecnológica.",retorno,"Técnico em Manutenção","Fechar");
 });
+
+$(document).on('click','#edific',function(){
+  navigator.notification.alert("O Técnico de Edificações é o profissional responsável por atuar com a elaboração de orçamentos, projetos e organização de obras ,Um Técnico de Edificações realiza levantamentos em obras rápidas corporativas de varejo, visando planejar a execução e elaboração de orçamentos.",retorno,"Técnico em Edificações","Fechar");
+});
+
