fd2553125dfec97b1e630e67b93ef7f1b5913087
yvolorenzo2019 <yvolorezo@gmail.com>
2019-04-28 12:14:52 +0900

    Modal Correto Manutenção

---
+++ www/js/modais.js
@@ -0,0 +1,8 @@
+
+function retorno()
+{
+
+}
+$(document).on('click','#opc2',function(){
+  navigator.notification.alert("O curso Técnico em Manutenção e Suporte em Informática tem por objetivo capacitar profissionais para fazer a instalação, configuração e manutenção de equipamentos de informática, a configuração e gerenciamento de redes de computadores, além de dar suporte na utilização dos mais diversos softwares e na identificação das necessidades de substituição ou atualização tecnológica.",retorno,"Técnico em Manutenção","Fechar");
+});
