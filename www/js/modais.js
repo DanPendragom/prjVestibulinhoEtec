ecfcb82e63e648acfad505810f45ceffa8547309
yvolorenzo2019 <yvolorezo@gmail.com>
2019-04-28 12:24:41 +0900

    Técnico em Turismo

--- www/js/modais.js
+++ www/js/modais.js
@@ -11,3 +11,8 @@ $(document).on('click','#edific',function(){
   navigator.notification.alert("O Técnico de Edificações é o profissional responsável por atuar com a elaboração de orçamentos, projetos e organização de obras ,Um Técnico de Edificações realiza levantamentos em obras rápidas corporativas de varejo, visando planejar a execução e elaboração de orçamentos.",retorno,"Técnico em Edificações","Fechar");
 });
 
+
+$(document).on('click','#turismo',function(){
+  navigator.notification.alert("O Técnico em Guia de Turismo é o profissional responsável por orientar e conduzir pessoas ou grupos durante traslados, passeios, visitas e viagens. Um Técnico em Guia de Turismo informa sobre aspectos socioculturais, históricos, ambientais, geográficos e outros de interesse do turista.",retorno,"Técnico em Turismo","Fechar");
+});
+
